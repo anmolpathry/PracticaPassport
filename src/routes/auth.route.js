b2047b94f29ebc12c92b77c721c25822254eaf73
anmolpathry <anmolitasweet@gmail.com>
2022-03-08 22:24:39 -0600

    cambio en rutas extras

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -41,23 +41,5 @@ router.get('/logout', (req, res) => {
   res.redirect('/');
 });
 
-/// path: profile/
-
-// GET /profile
-router.get('/profile', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/html/profile.html'));
-  //console.log("hola");
-  console.log(req.user);
-});
-
-//Authenticated USER
-router.get('/authUser', (req, res) =>{
-  if(req.user){
-    const user = {user: req.user};
-    res.status(200).send(user);
-  }else{
-    res.status(401).send('Unauthenticated User');
-  }
-})
 
 module.exports = router;
